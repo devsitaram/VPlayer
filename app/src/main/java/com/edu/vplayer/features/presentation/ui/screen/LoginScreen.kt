1634c6b913a5af8aab576ab04ab7e1b45a710bff
Ram Nepali <pariyarram056@gmail.com>
2023-09-08 15:00:45 +0545

    remove code Componentes

--- app/src/main/java/com/edu/vplayer/features/presentation/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/edu/vplayer/features/presentation/ui/screen/LoginScreen.kt
@@ -2,17 +2,36 @@ package com.edu.vplayer.features.presentation.ui.screen
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.edu.vplayer.features.presentation.ui.components.TextView
 
 @Composable
 fun LoginViewScreen(navHostController: NavHostController) {
 
-    TextView(text = "Hello")
+
 
 }
--- app/src/main/java/com/edu/vplayer/features/presentation/ui/screen/VideoScreen.kt
+++ app/src/main/java/com/edu/vplayer/features/presentation/ui/screen/VideoScreen.kt
@@ -1,13 +1,23 @@
 package com.edu.vplayer.features.presentation.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import com.edu.vplayer.R
 import com.edu.vplayer.features.presentation.ui.components.ButtonView
 
@@ -22,4 +32,5 @@ fun VideoViewScreen() {
         text = "",
         textStyle = TextStyle()
     )
+
 }
